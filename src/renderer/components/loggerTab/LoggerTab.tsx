7b57b7e0e1304d2e043d51b5561e791186868f0f
andrey_s <sudarev.andrey.i@gmail.com>
2024-01-21 11:52:16 +0300

    [renderer] upd ui LoggerTab.tsx - add TextArea, upd useEffect

--- src/renderer/components/loggerTab/LoggerTab.tsx
+++ src/renderer/components/loggerTab/LoggerTab.tsx
@@ -2,22 +2,31 @@ import React, { FC, useEffect, useState } from 'react';
 import './loggerTab.scss';
 import TabContainer from '../shared/tabContainer/TabContainer.tsx';
 import { LOGGER_ADD } from '../../../app/events/constants.ts';
+import TextArea from '../shared/textarea/TextArea.tsx';
 
 export const LoggerTab: FC = () => {
-  const [log, setLog] = useState([]);
+  const [logs, setLogs] = useState<string[]>([]);
 
   useEffect(() => {
     window.ipcRenderer.on(LOGGER_ADD, (_event, message) => {
-      setLog(prev => prev ? `${prev}\n${message}` : message);
+      setLogs(prev => {
+        return prev.length > 1000 ?
+          [prev.slice(1), message] :
+          [...prev, message];
+      });
     });
   }, []);
 
 
   return (
     <TabContainer name={'logger'}>
-      <>
-        <textarea value={log}/>
-      </>
+      <div style={{ height: '150px' }}>
+        <TextArea
+          text={logs.join('\n')}
+          autoScrolling={true}
+        />
+      </div>
+
     </TabContainer>
   );
 };
