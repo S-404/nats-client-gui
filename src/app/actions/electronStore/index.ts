c616a011f4f9d04a02a2c0d5a4aff6db2ba0ac34
andrey_s <sudarev.andrey.i@gmail.com>
2024-01-21 20:22:52 +0300

    [main] fix action argument type

--- src/app/actions/electronStore/index.ts
+++ src/app/actions/electronStore/index.ts
@@ -2,7 +2,7 @@ import { IpcRendererEvent } from 'electron';
 import store from '../../stores/electronStore.ts';
 
 const electronStoreActions = {
-  storeSave: (_: IpcRendererEvent, message: { key: string, value: string | number }) => store.set(message),
+  storeSave: (_: IpcRendererEvent, message: { [key: string]: string }) => store.set(message),
   storeGet: (_: IpcRendererEvent, message: string) => store.get(message),
   storeDelete: (_: IpcRendererEvent, message: string) => store.delete(message),
 };
