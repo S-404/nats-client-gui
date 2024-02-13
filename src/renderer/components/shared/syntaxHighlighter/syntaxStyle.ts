533130fbf50f81139bd5cc1cabb6fedf2da146b1
andrey_s <sudarev.andrey.i@gmail.com>
2024-02-13 20:24:43 +0300

    [renderer] fix syntaxStyleOverride

--- src/renderer/components/shared/syntaxHighlighter/syntaxStyle.ts
+++ src/renderer/components/shared/syntaxHighlighter/syntaxStyle.ts
@@ -2,16 +2,7 @@ import { tomorrowNightBright as style } from 'react-syntax-highlighter/dist/esm/
 
 const syntaxStyleOverride = {
   'hljs-string': {
-    'color': '#f9fde3'
-  },
-  'hljs-symbol': {
-    'color': '#f9fde3'
-  },
-  'hljs-bullet': {
-    'color': '#f9fde3'
-  },
-  'hljs-addition': {
-    'color': '#f9fde3'
+    'color': '#b2b4b4',
   },
 };
 
