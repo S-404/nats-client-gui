2c080b3802d313a08fe558f08c2260ed2c777f8c
andrey_s <sudarev.andrey.i@gmail.com>
2024-03-07 15:56:56 +0300

    [renderer] add saved subjects filter

--- src/renderer/components/subjectsTab/savedSubjects/SavedSubjectsModal.tsx
+++ src/renderer/components/subjectsTab/savedSubjects/SavedSubjectsModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import Subject from '#renderer/components/subjectsTab/subject/Subject.tsx';
 import NatsClientStore, { SubjectItem } from '#renderer/store/NatsClientStore.ts';
 import { appActionDispatcher } from '#renderer/bridge';
@@ -6,6 +6,7 @@ import SavedSubjectsStore from '#renderer/store/SavedSubjectsStore.ts';
 import { observer } from 'mobx-react';
 import Modal from '#renderer/components/shared/modal/Modal.tsx';
 import IconButton from '#renderer/components/shared/buttons/iconButton/IconButton.tsx';
+import MyInput from '#renderer/components/shared/inputs/myInput/MyInput.tsx';
 
 import './savedSubjects.scss';
 
@@ -16,6 +17,18 @@ interface ISavedSubjectsModalProps {
 
 const SavedSubjectsModal: FC<ISavedSubjectsModalProps> = observer(({ isModalOpened, closeModal }) => {
   const { savedSubjects } = SavedSubjectsStore;
+
+  const [filter, setFilter] = useState('');
+
+  const filteredSubjects = useMemo(() => {
+    if (!filter) {
+      return savedSubjects;
+    }
+    return savedSubjects.filter(item => {
+      return item.name.toLowerCase().includes(filter.toLowerCase()) ||
+        item.method.toLowerCase().includes(filter.toLowerCase());
+    });
+  }, [filter, savedSubjects]);
 
   const removeFromStore = (id: string) => {
     SavedSubjectsStore.removeSavedSubject(id);
@@ -53,21 +66,32 @@ const SavedSubjectsModal: FC<ISavedSubjectsModalProps> = observer(({ isModalOpen
       title={'Load Subject'}
     >
       <div className={'saved-subjects'}>
-        {savedSubjects.map((item) => (
-          <div
-            key={item.id}
-            className={'saved-subjects__subject'}
-          >
-            <Subject
-              onClick={() => loadSubjectFromStore(item)}
-              {...item}
-            />
-            <div className={'subject__remove-button'}>
-              <IconButton onClick={() => removeFromStore(item.id)} iconType={'remove'}/>
-            </div>
-          </div>
-
-        ))}
+        <div className={'saved-subjects__search'}>
+          <MyInput
+            text={filter}
+            title={'search'}
+            onChange={(e) => setFilter(e.target.value)}
+          />
+        </div>
+        <div className={'saved-subjects__subjects'}>
+                      {filteredSubjects.map((item) => (
+                        <div
+                          key={item.id}
+                          className={'subjects__subject-item'}
+                        >
+                          <Subject
+                            onClick={() => loadSubjectFromStore(item)}
+                            {...item}
+                          />
+                          <div className={'subject-item__remove-button'}>
+                            <IconButton onClick={() => removeFromStore(item.id)} iconType={'remove'}/>
+                          </div>
+                        </div>
+                      ))}
+        </div>
+        {!filteredSubjects.length && (
+          <p>Not found</p>
+        )}
       </div>
     </Modal>
   );
